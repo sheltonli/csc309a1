f9ef8b3b28a95d2013860fa568618b6d5200fcbc
Will <william.g.armstrong@gmail.com>
2014-01-31 12:47:35 -0500

    trying to merge

---
+++ scripts/collision.js
@@ -0,0 +1,5 @@
+for (i=0; i < bullets.length; i++) {
+	for (j=0; j<invaders.length; j++) {
+		if (invaders[i].x - 5 < bullets[i].x && invaders[i].y - 5 < bullets[i].y &&
+	}
+}
--- scripts/run.js
+++ scripts/run.js
@@ -5,6 +5,7 @@ function draw () {
 
 setInterval(function () {
 	updateInvaders();
+	updateCollisions();
 	updateTank();
 	ctx.clearRect(0,0, 600, 600);
 	draw();
